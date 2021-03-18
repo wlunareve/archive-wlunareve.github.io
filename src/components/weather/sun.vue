63e4bbeca7b28fad64caccde9b0e54b36cb73f7b
ToD <wlunareve@gmail.com>
2021-03-18 22:49:01 +0800

    feat: add hill and sun

--- src/components/weather/sun.vue
+++ src/components/weather/sun.vue
@@ -1,5 +1,7 @@
 <template>
-  {{ props_object }}
+  <div id='sun'>
+
+  </div>
 </template>
 
 <script setup>
@@ -7,17 +9,57 @@ import { defineProps, reactive } from "vue";
 
 // setup 中的 defineProps 與 vue2 的 props 只能用一個
 defineProps({
-  props_object: {
-    type: Object,
-    default: () => ({}),
-  },
+  
 });
 
 const state = reactive({ count: 0 });
 </script>
 
 <style lang="scss" scoped>
-a {
-  color: red;
-}
+  #sun {
+    position: absolute;
+    margin: 3rem;
+    top: 0;
+    width: 200px;
+    height: 200px;
+    background-color: #FFDE00;
+    border-radius: 50%;
+    box-shadow:
+      0 0 0 20px #FFDE0080,
+      0 0 0 40px #FFDE0040,
+      0 0 0 60px #FFDE0020,
+      0 0 0 80px #FFDE0010,
+      0 0 0 100px #FFDE0000,
+      0 0 40px 100px #FFDE0010;
+    animation:
+      sunrise 2s infinite linear forwards,
+      rays 2s 2s infinite linear;
+  }
+
+  @keyframes sunrise {
+    0% {
+      box-shadow: none;
+    }
+  }
+
+  @keyframes rays {
+    0% {
+      box-shadow: 
+      0 0 0 0 #FFDE0080,
+      0 0 0 20px #FFDE0080,
+      0 0 0 40px #FFDE0040,
+      0 0 0 60px #FFDE0020,
+      0 0 0 80px #FFDE0010,
+      0 0 40px 100px #FFDE0010;
+    }
+    100% {
+      box-shadow: 
+      0 0 0 20px #FFDE0080,
+      0 0 0 40px #FFDE0040,
+      0 0 0 60px #FFDE0020,
+      0 0 0 80px #FFDE0010,
+      0 0 0 100px #FFDE0000,
+      0 0 40px 100px #FFDE0010;
+    }
+  }
 </style>
--- src/views/main.vue
+++ src/views/main.vue
@@ -1,7 +1,22 @@
 <template>
-  <Sun 
-    :props_object="test_data"> 
-  </Sun>
+  <div class="outer-wrapper">
+    <div class="wrapper"> 
+      <div class="slide spring">
+        <Sun />
+        <div v-for="(item, index) in hills" :key="index" class="hill hill--spring" />
+        <!-- <div class="mountain" /> -->
+      </div>
+      <div class="slide summer">
+        <div v-for="(item, index) in hills" :key="index" class="hill hill--summer" />
+      </div>
+      <div class="slide fall">
+        <div v-for="(item, index) in hills" :key="index" class="hill hill--fall" />
+      </div>
+      <div class="slide winter">
+        <div v-for="(item, index) in hills" :key="index" class="hill hill--winter" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -16,15 +31,73 @@ export default {
 
   data() {
     return {
-      test_data: {
-        'a': {
-          'b': 1
-        },
-        'c': {
-          'd': 2
-        }
-      }
+      hills: [...Array(10)]
     }
   }
 };
 </script>
+
+<style lang="scss" scoped>
+  .outer-wrapper {
+    width: 100vh;
+    height: 100vw;
+    transform: rotate(-90deg) translateX(-100vh);
+    transform-origin: top left;
+    overflow-y: scroll;
+    overflow-x: hidden;
+    position: absolute;
+    scrollbar-width: none;
+
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
+  
+
+  .wrapper {
+    display: flex;
+    flex-direction: row;
+    width: 400vw;
+    transform: rotate(90deg) translateY(-100vh);
+    transform-origin: top left;
+  }
+
+  .slide {
+    width: 100vw;
+    height: 100vh;
+    background:  beige;
+    display: flex;
+    align-items: flex-end;
+    font-size: xx-large;
+  }
+
+  .spring {
+    // background: linear-gradient(0deg, #A4CF66, #00B9F2)
+  }
+
+  .hill {
+    position: relative;
+    left: 0;
+    bottom: 0;
+    width: 10vw;
+    height: 10vh;
+    border-radius: 100% 100% 0 0;
+
+    &--spring {
+      background: #779E36;
+    }
+
+    &--summer {
+      background: green;
+    }
+
+    &--fall {
+      background: #B56228;
+    }
+
+    &--winter {
+      background: #634329;
+    }
+  }
+</style>
+
