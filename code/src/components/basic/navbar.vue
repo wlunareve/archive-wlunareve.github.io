7209b219e52d3d57494efbca01708a75963414a0
ToD <wlunareve@gmail.com>
2021-05-13 23:50:37 +0800

    feat(navbar): add external link

--- code/src/components/basic/navbar.vue
+++ code/src/components/basic/navbar.vue
@@ -2,17 +2,35 @@
   <nav class="nav">
     <div class="nav__container">
       <a href="/" class="nav__title">ToD</a>
+      <div class="nav__link">
+        <a v-for="({ name, link }) in links" :key="name" 
+          :href="link" 
+          target="_blank" rel="noopener noreferrer"
+        >
+          <i class="icon fab"
+            :class="name"
+          ></i>
+        </a>
+      </div>
     </div>
   </nav>
 </template>
   
 <script>
   export default {
-    name: "main",
+    name: "navbar",
 
-    components: {
-      
-    },
+    setup() {
+      const links = [
+        { name:  'fa-github', link: 'https://github.com/wlunareve'},
+        { name:  'fa-linkedin-in', link: 'https://www.linkedin.com/in/%E6%98%8E%E8%AC%99-%E5%AE%8B-982b30172/'},
+        { name:  'fa-instagram', link: 'https://www.instagram.com/sung35680/?hl=zh-tw'},
+      ]
+
+      return {
+        links
+      }
+    }
 };
 </script>
 
@@ -21,10 +39,12 @@
     width: 100vw;
     min-height: 3.25rem;
     position: fixed;
+    left: 0;
     border-bottom: 1px solid #dbdbdb;
     display: flex;
     justify-content: center;
     align-items: center;
+    background: white;
 
     &__title {
       font-size: 1.5rem;
@@ -33,10 +53,22 @@
 
     &__container {
       width: 1680px;
+      display: flex;
+      justify-content: space-between;
+    }
+
+    &__link {
+      display: flex;
+      align-items: center;
     }
   }
 
   .nav::hover {
     background-color: #f9f8f7
+  }
+
+  .icon {
+    font-size: 1.5rem;
+    padding: 0 1rem;
   }
 </style>
