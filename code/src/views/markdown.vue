bf536b228dfed770629fa5e7573e0390ef075573
ToD <wlunareve@gmail.com>
2021-07-23 00:14:07 +0800

    feat(markdown): list style

--- code/src/views/markdown.vue
+++ code/src/views/markdown.vue
@@ -1,21 +1,39 @@
 <template>
   <!-- list -->
-  <div id="markdown">
-    <div class="list">
-      <article v-for="(markdown, index) in markdownList"
-        :key="index"
+  <div id="markdown" 
+    class="markdown__wrapper"
+  >
+    <div class="markdown">
+      <article v-for="(markdown) in markdownList"
+        :key="markdown.title"
+        class="article"
       >
-        <header class="list__title">
-          {{ markdown.title }}
-          {{ markdown.time }}
-        </header>
-        <p class="list__introduction">
-          {{ markdown.introduction }}
-        </p>
-        {{ markdown.path }}
-        <a :href="markdown.path">
-          詳細...
-        </a>
+        <div
+          class="article__left-side"
+        >
+          <router-link 
+            :to="`/markdown/${markdown.path}`" 
+          >
+            <span class="article__title">
+              {{ markdown.title }}
+            </span>
+            <p class="article__introduction">
+              {{ markdown.introduction }}
+            </p>
+          </router-link>
+
+          <div class="label">
+            <span v-for="(label) in markdown.labels"
+              :key="label"
+              class="label__item"
+            >
+              {{ label }}
+            </span>
+          </div>
+        </div>
+        <span class="article__time">
+          最後修改時間：{{ markdown.time }}
+        </span>
       </article>
     </div>
   </div>
@@ -33,6 +51,8 @@
       HelloWorld
     },
 
+    // TODO
+
     setup() {
       console.log(markdownList)
 
@@ -43,3 +63,57 @@
   };
 </script>
 
+<style lang="scss" scoped>
+  @import '../css/color';
+
+  .markdown {
+    margin: 0 auto;
+    width: 1280px;
+  }
+
+  .article {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);;
+    margin: 1rem 1rem 2rem;
+    color: rgb(117, 117, 117);
+    // flex-direction: column;
+    // align-items: flex-end;
+    // justify-content: space-between;
+
+    &__left-side {
+      grid-column-start: 1;
+      grid-column-end: 4;
+    }
+
+    &__title {
+      font-size: 1.5rem;
+      font-weight: 700;
+      line-height: 2rem;
+      color: rgb(41, 41, 41);
+    }
+
+    &__introduction {
+      display: -webkit-box;
+      overflow: hidden;
+      margin: .5rem 0;
+      height: 3rem;
+      text-overflow: ellipsis;
+      -webkit-line-clamp: 2;
+      -webkit-box-orient: vertical;
+    }
+
+    &__time {
+      align-self: flex-end;
+      font-size: .8rem;
+    }
+  }
+
+  .label {
+    &__item {
+      padding: .125rem .5rem;
+      background-color: rgb(242, 242, 242);
+      border-radius: 100px;
+      cursor: pointer;
+    }
+  }
+</style>
