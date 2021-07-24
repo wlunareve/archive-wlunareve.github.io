bef9ccace2f42fc4f129be698d66e005e82ad112
ToD <wlunareve@gmail.com>
2021-07-25 00:24:15 +0800

    refactor: move markdown file to static directory

--- code/vite.config.js
+++ code/vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': pathResolve('src'),
-      '@md': pathResolve('markdown')
+      '@md': pathResolve('static/markdown')
     }
   },
   build: {
